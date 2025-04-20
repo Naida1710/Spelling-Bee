9583ba0b6e3b45fd70347a2c991102d77ad29a0c
Naida Hadzic <naida.popovic1993@gmail.com>
2025-04-20 18:14:40 +0200

    Add event listeners for Start Quiz are and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const difficultyButtons = document.querySelectorAll(".difficulty-btn"); 
 
-    const difficultySelection = document.getElementById("difficulty-selection"); 
+    const difficultySelection = document.getElementById("difficulty-selection");
+    const startQuizSection = document.getElementById("start-quiz"); 
+
+    const startQuizButton = document.getElementById("startQuiz"); 
   });
