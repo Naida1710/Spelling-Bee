29c3ad06ac0cfbfc054c5e5ac3366bbdda6e411f
Naida Hadzic <naida.popovic1993@gmail.com>
2025-04-20 19:06:48 +0200

    Add formatTime function to convert seconds into MM:SS format

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,3 +183,14 @@ document.getElementById("question-number").textContent = "0";
 document.getElementById("score").textContent = "0"; 
 
 }); 
+function formatTime(seconds) { 
+
+    const mins = Math.floor(seconds / 60).toString().padStart(2, '0'); 
+    
+    const secs = (seconds % 60).toString().padStart(2, '0'); 
+    
+    return `${mins}:${secs}`; 
+    
+     
+    
+    } 
